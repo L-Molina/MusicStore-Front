85cc838dbfb53a7d17d3500823807fb177e25b95
L-Molina <lautarogabrielmolina@gmail.com>
2026-05-06 09:13:21 -0300

    delete copies

--- src/hooks/useCart 2.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { CartContext } from '../context/cart-context.js'
-
-export function useCart() {
-  const ctx = useContext(CartContext)
-  if (!ctx) throw new Error('useCart debe usarse dentro de CartProvider')
-  return ctx
-}
